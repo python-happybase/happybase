914f88a2317a48914f3f522af1c9214acb951324
Wouter Bolsterlee <uws@xs4all.nl>
2013-11-06 23:12:37 +0100

    Coding style cleanups in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from os.path import join, dirname
 from setuptools import find_packages, setup
 
+__version__ = None
 execfile('happybase/_version.py')
 
 
@@ -19,23 +20,24 @@ def get_install_requires():
     return install_requires
 
 
-setup(name='happybase',
-      version=__version__,
-      description="A developer-friendly Python library to interact "
-                  "with Apache HBase",
-      long_description=get_file_contents('README.rst'),
-      author="Wouter Bolsterlee",
-      author_email="uws@xs4all.nl",
-      url='https://github.com/wbolster/happybase',
-      install_requires=get_install_requires(),
-      packages=find_packages(exclude=['tests']),
-      license="MIT",
-      classifiers=(
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python :: 2",
-          "Topic :: Database",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          )
-      )
+setup(
+    name='happybase',
+    version=__version__,
+    description="A developer-friendly Python library to interact with "
+                "Apache HBase",
+    long_description=get_file_contents('README.rst'),
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url='https://github.com/wbolster/happybase',
+    install_requires=get_install_requires(),
+    packages=find_packages(exclude=['tests']),
+    license="MIT",
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    )
+)
