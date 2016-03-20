f61a5ec420c1c4cad250bddc180a63bc87bfcb38
Wouter Bolsterlee <uws@xs4all.nl>
2016-03-20 22:51:28 +0100

    Add ensure_bytes() helper
    ...to coerce unicode text into UTF-8 encoded byte strings.

--- happybase/util.py
+++ happybase/util.py
@@ -6,6 +6,7 @@ These functions are not part of the public API.
 
 import re
 
+import six
 from six.moves import range
 
 CAPITALS = re.compile('([A-Z])')
@@ -54,6 +55,18 @@ def thrift_type_to_dict(obj):
     """Convert a Thrift data type to a regular dictionary."""
     return dict((camel_case_to_pep8(attr), getattr(obj, attr))
                 for attr in thrift_attrs(obj))
+
+
+def ensure_bytes(str_or_bytes, binary_type=six.binary_type,
+                 text_type=six.text_type):
+    """Convert text into bytes, and leaves bytes as-is."""
+    if isinstance(str_or_bytes, binary_type):
+        return str_or_bytes
+    if isinstance(str_or_bytes, text_type):
+        return str_or_bytes.encode('utf-8')
+    raise TypeError(
+        "input must be a text or byte string, got {}"
+        .format(type(str_or_bytes).__name__))
 
 
 def str_increment(s):
