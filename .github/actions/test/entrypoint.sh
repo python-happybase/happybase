c9cab442c2e64831077651c272919ac4cc98ca65
aiudirog <aiudirog@gmail.com>
2019-11-26 23:47:48 -0500

    Give HBase more time to start up

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -3,5 +3,5 @@
 set -xe
 
 /opt/hbase-server & > /dev/null  # It's already got its own logs
-sleep 3  # Give it time to start up
+sleep 10  # Give it time to start up
 /usr/bin/python${PYTHON_VERSION} -m unittest  # TEST
