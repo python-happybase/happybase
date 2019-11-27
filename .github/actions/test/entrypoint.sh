e37ac2c50a57838e3b4ede2a257c9de0db572984
aiudirog <aiudirog@gmail.com>
2019-11-27 00:18:17 -0500

    Wait for thift port to bind instead of just 10s

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -3,5 +3,7 @@
 set -xe
 
 /opt/hbase-server & > /dev/null  # It's already got its own logs
-sleep 10  # Give it time to start up
+# Wait for thrift port to bind
+while ! netstat -tna | grep 'LISTEN\>' | grep -q ':9090\>'; do sleep 1; done
+sleep 1  # Give it a little extra time
 /usr/bin/python${PYTHON_VERSION} -m unittest  # TEST
