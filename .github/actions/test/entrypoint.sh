9cf3405359216444c591e64c0a411486b05da466
aiudirog <aiudirog@gmail.com>
2019-11-27 23:51:56 -0500

    Add coverage to test workflow

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -11,3 +11,10 @@ ${PYTHON} -m pip install -U -r test-requirements.txt
 while ! netstat -tna | grep 'LISTEN\>' | grep -q ':9090\>'; do sleep 1; done
 sleep 1  # Give it a little extra time
 ${PYTHON} -m unittest  # TEST
+
+if [ "${PYTHON_VERSION}" == "3.7" ]; then
+  ${PYTHON} -m pip install coverage codecov
+  ${PYTHON} -m coverage run -m unittest
+  ${PYTHON} -m coverage xml
+  ${PYTHON} -m codecov --token=${CODECOV_TOKEN}
+fi
