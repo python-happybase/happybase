512fca216b3d9d247f6538a2c92f889c8e2d18b1
aiudirog <aiudirog@gmail.com>
2019-11-27 22:58:42 -0500

    Update docker test to hopefully only do one job at a time

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -xe
 
-/opt/hbase-server & > /dev/null  # It's already got its own logs
+${PYTHON} -m pip install -U -r test-requirements.txt
+
+/opt/hbase-server & &> /dev/null  # It's already got its own logs
 # Wait for thrift port to bind
 while ! netstat -tna | grep 'LISTEN\>' | grep -q ':9090\>'; do sleep 1; done
 sleep 1  # Give it a little extra time
-/usr/bin/python${PYTHON_VERSION} -m unittest  # TEST
+${PYTHON} -m unittest  # TEST
